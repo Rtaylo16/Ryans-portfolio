5f8dabffa3f024d907ed754996a9234a961c4ac0
Rtaylo16 <59778940+Rtaylo16@users.noreply.github.com>
2020-11-03 14:40:44 -0500

    commit v2

---
+++ ryan-portfolio211/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+function App() {
+  return (
+  
+  );
+}
+
+export default App;
---
+++ ryan-portfolio211/src/mainpage.js
@@ -0,0 +1,407 @@
+import React from "react";
+
+/*#__PURE__*/
+React.createElement("div", null, /*#__PURE__*/React.createElement("meta", {
+  charSet: "UTF-8"
+}), /*#__PURE__*/React.createElement("meta", {
+  name: "viewport",
+  content: "width=device-width, initial-scale=1.0"
+}), /*#__PURE__*/React.createElement("link", {
+  rel: "stylesheet",
+  href: "assets/css/styles.css"
+}), /*#__PURE__*/React.createElement("link", {
+  rel: "stylesheet",
+  href: "https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css",
+  integrity: "sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2",
+  crossOrigin: "anonymous"
+}), /*#__PURE__*/React.createElement("link", {
+  href: "https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css",
+  rel: "stylesheet"
+}), /*#__PURE__*/React.createElement("title", null, "Portfolio website complete"), /*#__PURE__*/React.createElement("header", {
+  className: "l-header"
+}, /*#__PURE__*/React.createElement("nav", {
+  className: "nav bd-grid"
+}, /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("a", {
+  href: "#",
+  className: "nav__logo"
+}, "Ryan")), /*#__PURE__*/React.createElement("div", {
+  className: "nav__menu",
+  id: "nav-menu"
+}, /*#__PURE__*/React.createElement("ul", {
+  className: "nav__list"
+}, /*#__PURE__*/React.createElement("li", {
+  className: "nav__item"
+}, /*#__PURE__*/React.createElement("a", {
+  href: "#home",
+  className: "nav__link active"
+}, "Home")), /*#__PURE__*/React.createElement("li", {
+  className: "nav__item"
+}, /*#__PURE__*/React.createElement("a", {
+  href: "#about",
+  className: "nav__link"
+}, "About")), /*#__PURE__*/React.createElement("li", {
+  className: "nav__item"
+}, /*#__PURE__*/React.createElement("a", {
+  href: "#skills",
+  className: "nav__link"
+}, "Skills")), /*#__PURE__*/React.createElement("li", {
+  className: "nav__item"
+}, /*#__PURE__*/React.createElement("a", {
+  href: "#work",
+  className: "nav__link"
+}, "Work")), /*#__PURE__*/React.createElement("li", {
+  className: "nav__item"
+}, /*#__PURE__*/React.createElement("a", {
+  href: "#achievements",
+  className: "nav__link"
+}, "Achievements")), /*#__PURE__*/React.createElement("li", {
+  className: "nav__item"
+}, /*#__PURE__*/React.createElement("a", {
+  href: "#contact",
+  className: "nav__link"
+}, "Contact")))), /*#__PURE__*/React.createElement("div", {
+  className: "nav__toggle",
+  id: "nav-toggle"
+}, /*#__PURE__*/React.createElement("i", {
+  className: "bx bx-menu"
+})))), /*#__PURE__*/React.createElement("main", {
+  className: "l-main"
+}, /*#__PURE__*/React.createElement("section", {
+  className: "home bd-grid",
+  id: "home"
+}, /*#__PURE__*/React.createElement("div", {
+  className: "home__data"
+}, /*#__PURE__*/React.createElement("h1", {
+  className: "home__title"
+}, /*#__PURE__*/React.createElement("br", null), "I'am ", /*#__PURE__*/React.createElement("span", {
+  className: "home__title-color"
+}, "Ryan"), /*#__PURE__*/React.createElement("br", null), "Full Stack Developer"), /*#__PURE__*/React.createElement("a", {
+  href: "mailto:Rtaylo211@gmail.com",
+  className: "button"
+}, "Contact")), /*#__PURE__*/React.createElement("div", {
+  className: "home__social"
+}, /*#__PURE__*/React.createElement("a", {
+  href: "https://www.linkedin.com/in/ryan-c-taylor211/",
+  className: "home__social-icon"
+}, /*#__PURE__*/React.createElement("i", {
+  className: "bx bxl-linkedin"
+})), /*#__PURE__*/React.createElement("a", {
+  href: true,
+  className: "home__social-icon"
+}, /*#__PURE__*/React.createElement("i", {
+  className: "bx bxl-behance"
+})), /*#__PURE__*/React.createElement("a", {
+  href: "https://github.com/Rtaylo16",
+  className: "home__social-icon"
+}, /*#__PURE__*/React.createElement("i", {
+  className: "bx bxl-github"
+}))), /*#__PURE__*/React.createElement("div", {
+  className: "home__img"
+}, /*#__PURE__*/React.createElement("img", {
+  src: "assets/img/ryan.png",
+  alt: ""
+}))), /*#__PURE__*/React.createElement("section", {
+  className: "about section ",
+  id: "about"
+}, /*#__PURE__*/React.createElement("h2", {
+  className: "section-title"
+}, "About"), /*#__PURE__*/React.createElement("div", {
+  className: "about__container bd-grid"
+}, /*#__PURE__*/React.createElement("div", {
+  className: "about__img"
+}, /*#__PURE__*/React.createElement("img", {
+  src: "assets/img/about.jpg",
+  alt: ""
+})), /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h2", {
+  className: "about__subtitle"
+}, "I'am Ryan Taylor"), /*#__PURE__*/React.createElement("p", {
+  className: "about__text"
+}, "Full Stack Developer who is obsessed with learning innovative ways to create applications. I am a highly creative individual who loves Graphic Design, Animation and Web Design."), /*#__PURE__*/React.createElement("br", null), /*#__PURE__*/React.createElement("a", {
+  className: "btn btn-primary",
+  href: "https://docs.google.com/document/d/14j8xQH_nzPEytOMvrRVtiRLhmm0HkhNSIIl0XUbLXbY/edit?usp=sharing",
+  role: "button",
+  id: "resume-btn"
+}, "Resume")))), /*#__PURE__*/React.createElement("section", {
+  className: "skills section",
+  id: "skills"
+}, /*#__PURE__*/React.createElement("h2", {
+  className: "section-title"
+}, "Skills", /*#__PURE__*/React.createElement("img", {
+  className: "animation1",
+  src: "assets/img/animat-pencil-color.gif",
+  onmouseover: "this.src='assets/img/animat-pencil-color.gif'",
+  onmouseout: "this.src='assets/img/animat-pencil-color.gif'",
+  alt: ""
+})), /*#__PURE__*/React.createElement("div", {
+  className: "skills__container bd-grid"
+}, /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h2", {
+  className: "skills__text"
+}, "Front End"), /*#__PURE__*/React.createElement("div", {
+  className: "skillpics"
+}, /*#__PURE__*/React.createElement("img", {
+  src: "assets/img/Front end Icons.png",
+  alt: ""
+}), /*#__PURE__*/React.createElement("h2", {
+  className: "skills__text"
+}, "Cloud"), /*#__PURE__*/React.createElement("img", {
+  src: "assets/img/cloudicons.png",
+  alt: ""
+}), /*#__PURE__*/React.createElement("h2", {
+  className: "skills__text"
+}, "Databases"), /*#__PURE__*/React.createElement("img", {
+  src: "assets/img/databaseicons.png",
+  alt: ""
+}), /*#__PURE__*/React.createElement("h2", {
+  className: "skills__text"
+}, "Server"), /*#__PURE__*/React.createElement("img", {
+  src: "assets/img/server icons.png",
+  alt: ""
+}))))), /*#__PURE__*/React.createElement("section", {
+  className: "work section",
+  id: "work"
+}, /*#__PURE__*/React.createElement("h2", {
+  className: "section-title"
+}, "Work", /*#__PURE__*/React.createElement("img", {
+  className: "animation1",
+  src: "assets/img/animat-responsive-color.gif",
+  alt: ""
+})), /*#__PURE__*/React.createElement("div", {
+  className: "work__container bd-grid"
+}, /*#__PURE__*/React.createElement("div", {
+  className: "work__img"
+}, /*#__PURE__*/React.createElement("img", {
+  src: "assets/img/mediancolwindow.png",
+  alt: ""
+}), /*#__PURE__*/React.createElement("button", {
+  type: "button",
+  className: "btn btn-primary btn-lg btn-block",
+  "data-toggle": "modal",
+  "data-target": "#staticBackdrop"
+}, "Learn More!")), /*#__PURE__*/React.createElement("div", {
+  className: "work__img"
+}, /*#__PURE__*/React.createElement("img", {
+  src: "assets/img/books.png",
+  alt: ""
+}), /*#__PURE__*/React.createElement("button", {
+  type: "button",
+  className: "btn btn-primary btn-lg btn-block",
+  "data-toggle": "modal",
+  "data-target": "#exampleModal"
+}, "Learn More!")), /*#__PURE__*/React.createElement("div", {
+  className: "work__img"
+}, /*#__PURE__*/React.createElement("img", {
+  src: "assets/img/fitness.png",
+  alt: ""
+}), /*#__PURE__*/React.createElement("button", {
+  type: "button",
+  className: "btn btn-primary btn-lg btn-block",
+  "data-toggle": "modal",
+  "data-target": "#exampleModal3"
+}, "Learn More!")))), /*#__PURE__*/React.createElement("section", {
+  className: "work section",
+  id: "achievements"
+}, /*#__PURE__*/React.createElement("h2", {
+  className: "section-title"
+}, "Achievements"), /*#__PURE__*/React.createElement("div", {
+  className: "certs"
+}, /*#__PURE__*/React.createElement("div", {
+  className: "card text-center"
+}, /*#__PURE__*/React.createElement("div", {
+  className: "card-header"
+}), /*#__PURE__*/React.createElement("div", {
+  className: "card-body"
+}, /*#__PURE__*/React.createElement("h5", {
+  className: "card-title"
+}, "Certifications and Achievements"), /*#__PURE__*/React.createElement("p", {
+  className: "card-text"
+}, "Please take a look at my certifications that I have been awarded."), /*#__PURE__*/React.createElement("a", {
+  href: "https://docs.google.com/document/d/1-Ouukor7tId0MDj6M-Udg6zcOg8xa7P1EmoTjdMew4E/edit?usp=sharing",
+  className: "btn btn-primary"
+}, "Go to Certifications")), /*#__PURE__*/React.createElement("div", {
+  className: "card-footer text-muted"
+})))), /*#__PURE__*/React.createElement("section", {
+  className: "contact section",
+  id: "contact"
+}, /*#__PURE__*/React.createElement("h2", {
+  className: "section-title"
+}, "Contact"), /*#__PURE__*/React.createElement("div", {
+  className: "contact__container bd-grid"
+}, /*#__PURE__*/React.createElement("form", {
+  action: "mailto:Rtaylo211@gmail.com",
+  className: "contact__form"
+}, /*#__PURE__*/React.createElement("input", {
+  type: "text",
+  placeholder: "Name",
+  className: "contact__input"
+}), /*#__PURE__*/React.createElement("input", {
+  type: "mail",
+  placeholder: "Email",
+  className: "contact__input"
+}), /*#__PURE__*/React.createElement("textarea", {
+  name: true,
+  id: true,
+  cols: 0,
+  rows: 10,
+  className: "contact__input",
+  defaultValue: ""
+}), /*#__PURE__*/React.createElement("input", {
+  type: "button",
+  defaultValue: "Submit",
+  className: "contact__button button",
+  action: "mailto:Rtaylo211@gmail.com"
+}))))), /*#__PURE__*/React.createElement("footer", {
+  className: "footer"
+}, /*#__PURE__*/React.createElement("p", {
+  className: "footer__title"
+}, "Ryan"), /*#__PURE__*/React.createElement("div", {
+  className: "footer__social"
+}, /*#__PURE__*/React.createElement("a", {
+  href: "#",
+  className: "footer__icon"
+}, /*#__PURE__*/React.createElement("i", {
+  className: "bx bxl-facebook"
+})), /*#__PURE__*/React.createElement("a", {
+  href: "#",
+  className: "footer__icon"
+}, /*#__PURE__*/React.createElement("i", {
+  className: "bx bxl-instagram"
+})), /*#__PURE__*/React.createElement("a", {
+  href: "#",
+  className: "footer__icon"
+}, /*#__PURE__*/React.createElement("i", {
+  className: "bx bxl-twitter"
+}))), /*#__PURE__*/React.createElement("p", null, "\xA9 2020 copyright all right reserved")), /*#__PURE__*/React.createElement("div", {
+  className: "modal fade",
+  id: "staticBackdrop",
+  "data-backdrop": "static",
+  "data-keyboard": "false",
+  tabIndex: -1,
+  "aria-labelledby": "staticBackdropLabel",
+  "aria-hidden": "true"
+}, /*#__PURE__*/React.createElement("div", {
+  className: "modal-dialog"
+}, /*#__PURE__*/React.createElement("div", {
+  className: "modal-content"
+}, /*#__PURE__*/React.createElement("div", {
+  className: "modal-header"
+}, /*#__PURE__*/React.createElement("h5", {
+  className: "modal-title",
+  id: "staticBackdropLabel"
+}, "Median Col"), /*#__PURE__*/React.createElement("button", {
+  type: "button",
+  className: "close",
+  "data-dismiss": "modal",
+  "aria-label": "Close"
+}, /*#__PURE__*/React.createElement("span", {
+  "aria-hidden": "true"
+}, "\xD7"))), /*#__PURE__*/React.createElement("div", {
+  className: "modal-body"
+}, /*#__PURE__*/React.createElement("img", {
+  src: "./assets/img/home.png"
+}), /*#__PURE__*/React.createElement("p", null, /*#__PURE__*/React.createElement("b", null, "Description:"), /*#__PURE__*/React.createElement("br", null), "Median Col is an application that allows the user to compare their city data with data of a city they would be interested in working in. This application is a full-stack application utilizing HTML/CSS/Bootstrap/React for the front-end. It follows a MVC design pattern with the use of Node, Express and it uses MongoDB as the database."), /*#__PURE__*/React.createElement("img", {
+  src: "./assets/img/tools-median.png",
+  alt: ""
+})), /*#__PURE__*/React.createElement("div", {
+  className: "modal-footer"
+}, /*#__PURE__*/React.createElement("a", {
+  className: "btn btn-primary",
+  href: "https://livingcity.herokuapp.com",
+  role: "button",
+  id: "resume-btn"
+}, "Live Site"), /*#__PURE__*/React.createElement("a", {
+  className: "btn btn-primary",
+  href: "https://github.com/Rtaylo16/Project-Third-",
+  role: "button"
+}, "Github"), /*#__PURE__*/React.createElement("button", {
+  type: "button",
+  className: "btn btn-secondary",
+  "data-dismiss": "modal"
+}, "Close"))))), /*#__PURE__*/React.createElement("div", {
+  className: "modal fade",
+  id: "exampleModal",
+  tabIndex: -1,
+  "aria-labelledby": "exampleModalLabel",
+  "aria-hidden": "true"
+}, /*#__PURE__*/React.createElement("div", {
+  className: "modal-dialog"
+}, /*#__PURE__*/React.createElement("div", {
+  className: "modal-content"
+}, /*#__PURE__*/React.createElement("div", {
+  className: "modal-header"
+}, /*#__PURE__*/React.createElement("h5", {
+  className: "modal-title",
+  id: "exampleModalLabel"
+}, "Google Book Search"), /*#__PURE__*/React.createElement("button", {
+  type: "button",
+  className: "close",
+  "data-dismiss": "modal",
+  "aria-label": "Close"
+}, /*#__PURE__*/React.createElement("span", {
+  "aria-hidden": "true"
+}, "\xD7"))), /*#__PURE__*/React.createElement("div", {
+  className: "modal-body"
+}, /*#__PURE__*/React.createElement("img", {
+  src: "./assets/img/book app.png",
+  alt: ""
+}), /*#__PURE__*/React.createElement("p", null, /*#__PURE__*/React.createElement("b", null, "Description:"), /*#__PURE__*/React.createElement("br", null), "This application allows the user to search books via google books api and save the book at the click of a button. In a business context this application allows consumers to search books before they buy from the google store or amazon."), /*#__PURE__*/React.createElement("img", {
+  src: "./assets/img/tools-median.png",
+  alt: ""
+})), /*#__PURE__*/React.createElement("div", {
+  className: "modal-footer"
+}, /*#__PURE__*/React.createElement("a", {
+  className: "btn btn-primary",
+  href: "https://googlebooks234567.herokuapp.com/",
+  role: "button",
+  id: "resume-btn"
+}, "Live Site"), /*#__PURE__*/React.createElement("a", {
+  className: "btn btn-primary",
+  href: "https://github.com/Rtaylo16/React-google-book-search",
+  role: "button"
+}, "Github"), /*#__PURE__*/React.createElement("button", {
+  type: "button",
+  className: "btn btn-secondary",
+  "data-dismiss": "modal"
+}, "Close"))))), /*#__PURE__*/React.createElement("div", {
+  className: "modal fade",
+  id: "exampleModal3",
+  tabIndex: -1,
+  "aria-labelledby": "exampleModalLabel",
+  "aria-hidden": "true"
+}, /*#__PURE__*/React.createElement("div", {
+  className: "modal-dialog"
+}, /*#__PURE__*/React.createElement("div", {
+  className: "modal-content"
+}, /*#__PURE__*/React.createElement("div", {
+  className: "modal-header"
+}, /*#__PURE__*/React.createElement("h5", {
+  className: "modal-title",
+  id: "exampleModalLabel3"
+}, "Fitness Tracker"), /*#__PURE__*/React.createElement("button", {
+  type: "button",
+  className: "close",
+  "data-dismiss": "modal",
+  "aria-label": "Close"
+}, /*#__PURE__*/React.createElement("span", {
+  "aria-hidden": "true"
+}, "\xD7"))), /*#__PURE__*/React.createElement("div", {
+  className: "modal-body"
+}, /*#__PURE__*/React.createElement("img", {
+  src: "./assets/img/fitness tracker.png"
+}), /*#__PURE__*/React.createElement("p", null, /*#__PURE__*/React.createElement("b", null, "Description:"), /*#__PURE__*/React.createElement("br", null), "The user will create and track daily workouts. The user will have the ability to log multiple exercises. The user will track: Workout name, type, weight, sets, and reps through input. In a business context consumers will be able to keep up with their fitness goals while simultaneously tracking workout progress."), /*#__PURE__*/React.createElement("img", {
+  src: "./assets/img/tools-fit.png"
+})), /*#__PURE__*/React.createElement("div", {
+  className: "modal-footer"
+}, /*#__PURE__*/React.createElement("a", {
+  className: "btn btn-primary",
+  href: "https://sleepy-sands-73166.herokuapp.com/",
+  role: "button",
+  id: "resume-btn"
+}, "Live Site"), /*#__PURE__*/React.createElement("a", {
+  className: "btn btn-primary",
+  href: "https://github.com/Rtaylo16/Fitness-Tracker-",
+  role: "button"
+}, "Github"), /*#__PURE__*/React.createElement("button", {
+  type: "button",
+  className: "btn btn-secondary",
+  "data-dismiss": "modal"
+}, "Close"))))));
